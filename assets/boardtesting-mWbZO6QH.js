c3ff35cfc469abe533dcb575740116a0976dff89
Chiu_Yi_Kai <simon81515815@gmail.com>
2024-08-13 04:11:27 +0800

    Updates

--- assets/boardtesting-mWbZO6QH.js
+++ assets/boardtesting-mWbZO6QH.js
@@ -1,6 +1,8 @@
 const n=`---\r
 title: 測試桌遊標題噗噗\r
 rating: 5\r
+date: "2023-05-15"\r
+category: BoardGame\r
 ---\r
 \r
 # 噗噗噗\r
---
+++ assets/testing-DN_GN7wZ.js
@@ -0,0 +1,13 @@
+const n=`---\r
+title: "測試標題噗噗"\r
+date: "2023-05-15"\r
+category: "Movies"\r
+rating: 5\r
+---\r
+\r
+# 噗噗噗\r
+\r
+## 文章測試\r
+\r
+asdjnoa danasd od ud ennrrrn  qwi  i en.ne i e\r
+`;export{n as default};
