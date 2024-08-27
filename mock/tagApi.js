8c6b256bcea00e190c68af9a90eb2bc655894802
Chiu_Yi_Kai <simon81515815@gmail.com>
2024-08-27 13:59:30 +0800

    推移下

--- mock/tagApi.js
+++ mock/tagApi.js
@@ -1,38 +1,37 @@
 // import { MockMethod } from "vite-plugin-mock";
-// import { tagsList } from "@/data/tagsList";
+import { tagsList } from "@/data/";
 
 const tags = [
-  // ...tagsList,
-  // { id: '1', name: '策略', parentId: 2, category: 'boardgame' },
+  ...tagsList]
+  // { id: '1', name: '策略', parentId: null, category: 'boardgame' },
   // { id: '2', name: '家庭', parentId: null, category: 'boardgame' },
-  // { id: '3', name: '派對', parentId: 4  , category: 'boardgame' },
-  // { id: '4', name: '科幻', parentId: 3, category: 'movie' },
-  // { id: '5', name: '動作', parentId: 3, category: 'movie' },
-  // 添加更多初始標籤...
-  { id: '1', name: '策略', parentId: null, category: 'boardgame', description: '' },
-  { id: '2', name: '家庭', parentId: null, category: 'boardgame', description: '' },
-  { id: '3', name: '派對', parentId: null, category: 'boardgame', description: '' },
-  { id: '4', name: '輕策略', parentId: '1', category: 'boardgame', description: '' },
-  { id: '5', name: '重策略', parentId: '1', category: 'boardgame', description: '' },
-  { id: '6', name: '兒童', parentId: '2', category: 'boardgame', description: '' },
-  { id: '7', name: '聚會', parentId: '3', category: 'boardgame', description: '' },
-  { id: '8', name: '科幻', parentId: null, category: 'movie', description: '' },
-  { id: '9', name: '動作', parentId: null, category: 'movie', description: '' },
-  { id: '10', name: '劇情', parentId: null, category: 'movie', description: '' },
-  { id: '11', name: '太空歌劇', parentId: '8', category: 'movie', description: '' },
-  { id: '12', name: '賽博朋克', parentId: '8', category: 'movie', description: '' },
-  { id: '13', name: '武俠', parentId: '9', category: 'movie', description: '' },
-  { id: '14', name: '冒險', parentId: '9', category: 'movie', description: '' },
-  { id: '15', name: '動畫', parentId: null, category: 'anime', description: '' },
-  { id: '16', name: '少年', parentId: '15', category: 'anime', description: '' },
-  { id: '17', name: '少女', parentId: '15', category: 'anime', description: '' },
-  { id: '18', name: '成人', parentId: '15', category: 'anime', description: '' },
-  { id: '19', name: '解謎', parentId: null, category: 'escapegame', description: '' },
-  { id: '20', name: '恐怖', parentId: null, category: 'escapegame', description: '' },
-  { id: '21', name: '雜項', parentId: null, category: 'various', description: '' },
-  { id: '22', name: '推理', parentId: '21', category: 'various', description: '' },
-
-]
+  // { id: '3', name: '派對', parentId: null , category: 'boardgame' },
+  // { id: '4', name: '科幻', parentId: null, category: 'movie' },
+  // { id: '5', name: '動作', parentId: null, category: 'movie' },
+  // // 添加更多初始標籤...
+  // { id: '1', name: '策略', parentId: null, category: 'boardgame', description: '' },
+  // { id: '2', name: '家庭', parentId: null, category: 'boardgame', description: '' },
+  // { id: '3', name: '派對', parentId: null, category: 'boardgame', description: '' },
+  // { id: '4', name: '輕策略', parentId: '1', category: 'boardgame', description: '' },
+  // { id: '5', name: '重策略', parentId: '1', category: 'boardgame', description: '' },
+  // { id: '6', name: '兒童', parentId: '2', category: 'boardgame', description: '' },
+  // { id: '7', name: '聚會', parentId: '3', category: 'boardgame', description: '' },
+  // { id: '8', name: '科幻', parentId: null, category: 'movie', description: '' },
+  // { id: '9', name: '動作', parentId: null, category: 'movie', description: '' },
+  // { id: '10', name: '劇情', parentId: null, category: 'movie', description: '' },
+  // { id: '11', name: '太空歌劇', parentId: '8', category: 'movie', description: '' },
+  // { id: '12', name: '賽博朋克', parentId: '8', category: 'movie', description: '' },
+  // { id: '13', name: '武俠', parentId: '9', category: 'movie', description: '' },
+  // { id: '14', name: '冒險', parentId: '9', category: 'movie', description: '' },
+  // { id: '15', name: '動畫', parentId: null, category: 'anime', description: '' },
+  // { id: '16', name: '少年', parentId: '15', category: 'anime', description: '' },
+  // { id: '17', name: '少女', parentId: '15', category: 'anime', description: '' },
+  // { id: '18', name: '成人', parentId: '15', category: 'anime', description: '' },
+  // { id: '19', name: '解謎', parentId: null, category: 'escapegame', description: '' },
+  // { id: '20', name: '恐怖', parentId: null, category: 'escapegame', description: '' },
+  // { id: '21', name: '雜項', parentId: null, category: 'various', description: '' },
+  // { id: '21', name: '推理', parentId: null, category: 'various', description: '' },
+// ]
 
 export default [
   //獲取所有標籤
